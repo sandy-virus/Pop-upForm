db6c39f7d811746b7692c355013c878dd2c496ae
Sandeep Kumar <sandeepkumarjb1234@gmail.com>
2021-01-31 13:55:27 +0530

    validation

--- javascript.js
+++ javascript.js
@@ -30,15 +30,24 @@ document.getElementById("pop-up").addEventListener("click", function () {
   var select = document.getElementById("select");
   var textarea = document.getElementById("message");
 
+  var category = document.getElementById("category");
+  var msg = document.getElementById("msg");
+  var fname = document.getElementById("fname");
+  var lname = document.getElementById("lname");
+  var phone = document.getElementById("phone");
+  var mail = document.getElementById("mail");
+
   function validate() {
     if (select.value.trim() == 0) {
-      document.getElementById("slt").style.visibility = "visible";
+      document.getElementById("category").style.visibility = "visible";
+      document.getElementById("select").style.borderColor = "red";
       select.focus();
       return false;
     }
-    else if (textarea.value.length <= 0) {
+    if (textarea.value.length <= 0) {
       textarea.focus();
-      document.getElementById("sl").style.visibility = "visible";
+      document.getElementById("msg").style.visibility = "visible";
+      document.getElementById("message").style.borderColor = "red";
       return false;
     } else {
       document
@@ -51,10 +60,13 @@ document.getElementById("pop-up").addEventListener("click", function () {
     return false;
   }
 
+  
+  
   function validatePage2() {
     if (firstName.value.trim() === "") {
       document.getElementById("fname").style.visibility = "visible";
       firstName.focus();
+      firstName.style.borderColor = "red";
       return false;
     }
     if (lastName.value.trim() === "") {
@@ -66,6 +78,7 @@ document.getElementById("pop-up").addEventListener("click", function () {
     if (email.value.trim() === "") {
       email.focus();
       document.getElementById("mail").style.visibility = "visible";
+      document.getElementById("email").style.borderColor = "red";
       return false;
     }
     if (telephone.value.trim() === "") {
@@ -83,4 +96,60 @@ document.getElementById("pop-up").addEventListener("click", function () {
         });
     }
     return false;
+  }
+
+  
+  select.addEventListener('click', categoryError);
+  function categoryError(){
+    if (select.value.trim() != 0) {
+      category.style.visibility = "hidden";
+      select.style.borderColor = "green";
+      return true;
+    }
+  }
+
+  textarea.addEventListener('textInput', categoryError);
+  function categoryError(){
+    if (textarea.value.length >= 1) {
+      msg.style.visibility = "hidden";
+      textarea.style.borderColor = "green";
+      return false;
+    }
+  }
+
+  
+  firstName.addEventListener('textInput', error);
+  function error(){
+    if (firstName.value.trim() != "") {
+      fname.style.visibility = "hidden";
+      firstName.style.borderColor = "green";
+      return true;
+    }
+  }
+
+  lastName.addEventListener('textInput', error2);
+  function error2(){
+    if (lastName.value.trim() != "") {
+      lname.style.visibility = "hidden";
+      lastName.style.borderColor = "green";
+      return true;
+    }
+  }
+
+  email.addEventListener('textInput', mailError);
+  function mailError(){
+    if (email.value.trim() != "") {
+      mail.style.visibility = "hidden";
+      email.style.borderColor = "green";
+      return true;
+    }
+  }
+
+  telephone.addEventListener('textInput', phoneError);
+  function phoneError(){
+    if (telephone.value.trim() != "") {
+      phone.style.visibility = "hidden";
+      telephone.style.borderColor = "green";
+      return true;
+    }
   }
